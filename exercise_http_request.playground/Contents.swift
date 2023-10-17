81f61ec7acd07e0a6e033dfa3dc77bffd1aa009a
Mahmoud Alwakeel <alwakeel@Mahmouds-MacBook-Pro.local>
2023-10-17 18:32:37 +0300

    request data

---
+++ exercise_http_request.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+import Foundation
+
+let littleLemon = "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/littleLemonSimpleMenu.json"
+
+let url = URL(string: littleLemon)
+
+guard let url = url else {
+    throw NSError()
+}
+
+let request = URLRequest(url: url)
+
+let task = URLSession.shared.dataTask(with: request) { data, response, error in
+    if let data = data,
+       let string = String(data: data, encoding: .utf8) {
+        print(string)
+    }
+}
+task.resume()
